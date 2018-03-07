ff8ab5de2603fb8dc53df570b3a804ad5517be7a
patrick.g.neris <patrick.g.neris@github.com>
2018-03-07 14:55:52 -0400

    classe calculadora

--- AulaGitJava/src/br/edu/ifro/Aula.java
+++ AulaGitJava/src/br/edu/ifro/Aula.java
@@ -16,6 +16,7 @@ public class Aula {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Vai se fuder!!!");
     }
     
 }
---
+++ AulaGitJava/src/br/edu/ifro/Calculatora.java
@@ -0,0 +1,20 @@
+package br.edu.ifro;
+/**** @author Neris, Patrick*/
+public class Calculatora {
+
+    public double soma (double x, double y){
+        return x+y;
+    }
+    
+    public double subtracao (double x, double y) {
+        return x-y;
+    }
+    
+    public double divisao (double x, double y) {
+        return x/y;
+    }
+    
+    public double multiplicacao (double x, double y) {
+        return x*y;
+    }
+}
