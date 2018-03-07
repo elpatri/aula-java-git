a0f5fd61ebb06496cd9834118eceb9e98b6eab51
patrick.g.neris <patrick.g.neris@github.com>
2018-03-07 15:13:07 -0400

    calculadora pronta

--- AulaGitJava/src/br/edu/ifro/Calculatora.java
+++ AulaGitJava/src/br/edu/ifro/Calculatora.java
@@ -17,5 +17,13 @@ public class Calculatora {
     public double multiplicacao (double x, double y) {
         return x*y;
     }
-    
+    public static void main (String []args){
+        double x = 10;
+        double y = 20;
+        Calculatora c = new Calculatora () ;
+        System.out.println("soma: " + c.soma (x, y));
+        System.out.println("subtracao: " + c.subtracao (x, y));
+        System.out.println("divisao: " + c.divisao (x, y));
+        System.out.println("multiplicacao: " + c.multiplicacao (x, y));
+    }
 }
